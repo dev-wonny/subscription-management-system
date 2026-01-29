4a6efad7a429b8b4892794727ece7eb4b00d5ff1
twonny <wonny@torderstay.com>
2026-01-30 00:34:54 +0900

    tt

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//     plugins: [react()],
+//     server: {
+//         proxy: {
+//             '/api': 'http://localhost:3000'
+//         }
+//     }
+// })
+
+
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/api': 'http://localhost:3000'
-        }
-    }
+    plugins: [react()]
 })
